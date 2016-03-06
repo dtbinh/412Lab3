b368c3ec72692fbbb665e137ed0ac7c156115187
Karl Parkinson <kparkins@ualberta.ca>
2016-03-06 15:35:00 -0700

    sort of working, but not really. Should add path planning.

--- src/RemoteArm.java
+++ src/RemoteArm.java
@@ -7,6 +7,7 @@ import lejos.hardware.port.MotorPort;
 import lejos.remote.ev3.RMIRegulatedMotor;
 import lejos.remote.ev3.RemoteEV3;
 import lejos.utility.Matrix;
+import lejos.utility.Delay;
 
 public class RemoteArm {
 	public static TrackerReader tracker;
@@ -27,33 +28,31 @@ public class RemoteArm {
 	}
 	
 	public Matrix initJacobian(){
+		double angleMove = 10.0;
 		double [][] jacob = {{0,0},{0,0}};
 		double uStart = tracker.x;
 		double vStart = tracker.y;
 		
-		System.out.println(uStart);
-		System.out.println(vStart);
-		
-		this.goToAngle(25, 0);
+		this.goToAngle(angleMove, 0);
 
 		double uEnd = tracker.x;
 		double vEnd = tracker.y;
 		
-		jacob[0][0] = (uEnd - uStart) / 45.0;
-		jacob[1][0] = (vEnd - vStart) / 45.0;
+		jacob[0][0] = (uEnd - uStart) / angleMove;
+		jacob[1][0] = (vEnd - vStart) / angleMove;
 		
 		this.goToAngle(0, 0);
 		
 		uStart = tracker.x;
 		vStart = tracker.y;
 		
-		this.goToAngle(0, 25);
+		this.goToAngle(0, angleMove);
 		
 		uEnd = tracker.x;
 		vEnd = tracker.y;
 		
-		jacob[0][1] = (uEnd - uStart) / 45.0;
-		jacob[1][1] = (vEnd - vStart) / 45.0;
+		jacob[0][1] = (uEnd - uStart) / angleMove;
+		jacob[1][1] = (vEnd - vStart) / angleMove;
 		
 		this.goToAngle(0, 0);
 		
@@ -85,28 +84,64 @@ public class RemoteArm {
 	}
 	
 	public void moveToTarget() {
-		double [][] t = {{tracker.targetx},{tracker.targety}};
-		Matrix target = new Matrix(t);
+		int MAX_INT = 50;
+		Matrix error, angles, J, Jupdate, deltax;
+		double currentx, currenty, e, eold, threshold;
 		
-		Matrix angles;
+		J = initJacobian();
+		error = new Matrix(2,1);
+		angles = new Matrix(2, 1);
+		
+		currentx = tracker.x;
+		currenty = tracker.y;
+		
+		error.set(0, 0, tracker.targetx - currentx);
+		error.set(1, 0, tracker.targety - currenty);
 		
 		try {
-			double [][] angl = {{m1.getTachoCount()}, {m2.getTachoCount()}};
-			angles = new Matrix(angl);
-			while (true) {
-				double [][] curr = {{tracker.x}, {tracker.y}};
-				Matrix current = new Matrix(curr);
-			
-				Matrix deltaX = J.solve(target.minus(current));
-				angles = angles.plus(deltaX);
-				double theta1 = Math.toDegrees(angles.get(0, 0));
-				double theta2 = Math.toDegrees(angles.get(1, 0));
-				this.goToAngle(theta1, theta2);
-			}
-		} catch (RemoteException e) {
-		// TODO Auto-generated catch block
+			angles.set(0, 0, m1.getTachoCount());
+			angles.set(1, 0, m2.getTachoCount());
+		} catch (RemoteException e1) {
+			// TODO Auto-generated catch block
 			this.closePorts();
-			e.printStackTrace();
+			e1.printStackTrace();
+		}
+		
+		//angles = J.inverse().times(error);
+		
+		
+		e = error.normF();
+		threshold = 20;
+		
+		try {
+			for (int i = 1; i<=MAX_INT && e > threshold; i++) {
+				currentx = tracker.x;
+				currenty = tracker.y;
+				
+				deltax = J.inverse().times(error);
+				angles.plusEquals(deltax.times(-0.01));
+				double theta1 = Math.toDegrees(angles.get(0, 0)) % 360;
+				double theta2 = Math.toDegrees(angles.get(1, 0)) % 360;
+				this.goToAngle(theta1, theta2);
+
+				error.set(0, 0, tracker.targetx - tracker.x);
+				error.set(1, 0, tracker.targety - tracker.y);
+				
+				eold = e;
+				e = error.normF();
+				
+				if (e > 10+eold) {
+					System.out.println("Broyden Update");
+					Matrix deltay = new Matrix(2,1);
+					deltay.set(0, 0, tracker.x - currentx);
+					deltay.set(1, 0, tracker.y - currenty);
+					
+					J.plusEquals((deltay.minus(J.times(deltax)).times(deltax.transpose())).times(1/deltax.transpose().times(deltax).get(0, 0)));
+				}
+			}
+		} catch (Exception exc) {
+			this.closePorts();
+			exc.printStackTrace();
 		}
 	}
 	
@@ -128,10 +163,16 @@ public class RemoteArm {
 		Button.waitForAnyPress();
 		
 		RemoteArm ra = new RemoteArm();
+		//System.out.println(tracker.targetx);
+		//System.out.println(tracker.targety);
 		
-		ra.J = ra.initJacobian();
+		//ra.J = ra.initJacobian();
+		//ra.J.print(System.out);
 		
-		ra.moveToTarget();
+		//while (true) {
+			//Button.waitForAnyPress();
+			ra.moveToTarget();
+		//}
 		
 		ra.closePorts();
 		
